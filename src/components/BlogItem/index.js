ca537b72ef6c667d0333d560befc30e73c60e219
saiteja-mandadi-99 <saitejamandadi99>
2024-09-10 12:35:10 +0000

    added Link to blogItem for Routing

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,22 +1,26 @@
+import {Link} from 'react-router-dom'
+
 import './index.css'
 
 const BlogItem = props => {
   const {blogData} = props
   const {id, imageUrl, topic, title, avatarUrl, author} = blogData
   return (
-    <div className="item-container">
-      <img className="item-image" src={imageUrl} alt={`item${id}`} />
+    <Link to={`/blog/${id}`} className="item-link">
+      <div className="item-container">
+        <img className="item-image" src={imageUrl} alt={`item${id}`} />
 
-      <div className="item-info">
-        <p className="item-topic">{topic}</p>
+        <div className="item-info">
+          <p className="item-topic">{topic}</p>
 
-        <p className="item-title">{title}</p>
-        <div className="author-info">
-          <img className="avatar" src={avatarUrl} alt={`avatar${id}`} />
-          <p className="author-name">{author}</p>
+          <p className="item-title">{title}</p>
+          <div className="author-info">
+            <img className="avatar" src={avatarUrl} alt={`avatar${id}`} />
+            <p className="author-name">{author}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
